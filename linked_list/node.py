27b06bc6c80a8af7f6a4dcdaa35f53582d71e150
j0eTheRipper <j0eTheRipper@protonmail.com>
2021-08-29 19:07:19 +0300

    added the Doubly-linked Node to later implement the doubly linked list

--- linked_list/node.py
+++ linked_list/node.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, data=None, next_pointer=None):
         self.value = data
         self.next = next_pointer
+
+
+class DoubleNode(Node):
+    def __init__(self, data=None, next_pointer=None, previous_pointer=None):
+        super().__init__(data=data, next_pointer=next_pointer)
+        self.previous_pointer = previous_pointer
