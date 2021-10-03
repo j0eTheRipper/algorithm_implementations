625a5202f5a86384b089257ae3149208d5e23f94
j0eTheRipper <j0eTheRipper@protonmail.com>
2021-10-03 10:14:04 +0300

    added __repr__ for easier debugging

--- linked_list/SinglyLinked/Node.py
+++ linked_list/SinglyLinked/Node.py
@@ -2,3 +2,6 @@ class SingleNode:
     def __init__(self, data=None, next_pointer=None):
         self.value = data
         self.next = next_pointer
+
+    def __repr__(self):
+        return str(self.value)
