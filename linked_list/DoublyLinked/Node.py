cb8bbe150cb36095a0a75f70847b91b9c206ac6b
j0eTheRipper <j0eTheRipper@protonmail.com>
2021-10-12 18:23:24 +0300

    added a repr dunder for node representation and debugging purpuses.

--- linked_list/DoublyLinked/Node.py
+++ linked_list/DoublyLinked/Node.py
@@ -5,3 +5,6 @@ class DoubleNode(SingleNode):
     def __init__(self, data=None, next_pointer=None, previous_pointer=None):
         super().__init__(data=data, next_pointer=next_pointer)
         self.back = previous_pointer
+
+    def __repr__(self):
+        return str(self.value)
