9973cf481664a40a501a51c72bac2f8767f4a2b1
j0eTheRipper <j0eTheRipper@protonmail.com>
2021-10-13 20:41:43 +0300

    Made the public variable private.

--- ADTs/deque/Deque.py
+++ ADTs/deque/Deque.py
@@ -3,29 +3,29 @@ from linked_list.DoublyLinked.LinkedList import DoublyLinkedList
 
 class Deque:
     def __init__(self, data):
-        self.deque = DoublyLinkedList(data)
+        self.__deque = DoublyLinkedList(data)
 
     def peek_front(self):
-        return self.deque.head.value
+        return self.__deque.head.value
 
     def peek_back(self):
-        return self.deque.tail.value
+        return self.__deque.tail.value
 
     def pop_front(self):
-        x = self.deque[0].value
-        del self.deque[0]
+        x = self.__deque[0].value
+        del self.__deque[0]
         return x
 
     def pop_back(self):
-        x = self.deque[len(self) - 1].value
-        del self.deque[len(self) - 1]
+        x = self.__deque[len(self) - 1].value
+        del self.__deque[len(self) - 1]
         return x
 
     def add_front(self, data):
-        self.deque.reverse_append(data)
+        self.__deque.reverse_append(data)
 
     def add_back(self, data):
-        self.deque.append(data)
+        self.__deque.append(data)
 
     def __len__(self):
-        return len(self.deque)
+        return len(self.__deque)
